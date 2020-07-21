2960d3ad75a3dd23ee87735f53fc4330faede8f5
Prince Bassey <basseyprince75@gmail.com>
2020-07-21 16:03:08 +0100

    Updated employee api

--- employee_schema/employeeSchema.js
+++ employee_schema/employeeSchema.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const employee_Schema = new Schema({
-    firstName: String,
-    lastName:String,
+    firstname: String,
+    lastname:String,
     email:String,
     salary:Number,
     dob:String
