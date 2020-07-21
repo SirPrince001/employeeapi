5a6f206acf43da39cb73edaf0908456f6de34dc6
Prince Bassey <basseyprince75@gmail.com>
2020-07-21 13:20:38 +0100

    Updated employee api

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-const express = require('express');
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
-const cor = require('cors')
+const cor = require("cors");
 
-const config_DB = require('./empolyee_db/database')
+const config_DB = require("./empolyee_db/database");
 
-const emp_route = require('./employee_route/employee')
+const emp_route = require("./employee_route/employee");
+require("dotenv").config();
 
 //connecting to Database
-mongoose.connect(config_DB.db , {useNewUrlParser:true , useUnifiedTopology:true}).then(() =>{
-    console.log('Connected to Database!!')
+mongoose
+  .connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(
+    () => {
+      console.log("Connected to Database!!");
+    },
+    (error) => {
+      console.log("Error connectiong to the database" + error);
+    }
+  );
 
-}, error => {
-    console.log('Error connectiong to the database' + error)
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cor());
+app.use(emp_route);
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(cor())
-app.use(emp_route)
-
-
-app.listen(5000 , () => {
-    console.log('Listening to port 5000');
-})
+app.listen(5000, () => {
+  console.log("Listening to port 5000");
+});
